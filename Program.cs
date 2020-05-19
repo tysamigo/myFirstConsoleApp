8a56e0fc61616729422010e8f21ccbada61fb34f
Tyler Ramage <tysamigo@gmail.com>
2020-05-19 19:53:41 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace myFirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Tyler!");
+        }
+    }
+}
